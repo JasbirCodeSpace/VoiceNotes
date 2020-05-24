5a4e95e0eea3d1c622987fe8e9904159cc25912d
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-24 19:17:29 +0530

    Added SignUpForm class

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import Users
+from django import forms
+from django.contrib.auth.models import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+	first_name = forms.CharField(max_length=50,required=True)
+	last_name = forms.CharField(max_length=50,required=False)
+	email = forms.EmailField(max_length=100,required=True)
+
+	class Meta(UserCreationForm.Meta):
+		model = User
+		fields = UserCreationForm.Meta.Fields+['first_name','last_name','email']
+
+	def save(self,commit=True):
+		user = super(SignUpForm,self).save(commit=False)
+		user.first_name = self.cleaned_data['first_name']
+		user.last_name = self.cleaned_data['last_name']
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
