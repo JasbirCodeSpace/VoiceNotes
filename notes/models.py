3619d2eddb4ce1b67aa88b2dc2e272c93e7c4c1d
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-24 19:56:05 +0530

    Added Notes model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Notes(models.Model):
+	user  = models.ForeignKey(User,on_delete = models.CASCADE)
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_created = models.DateTimeField(default=timezone.now,auto_now_add = True)
+	date_modified = models.DateTimeField(default=timezone.now,auto_now = True)
+
+	def __str__(self):
+		retuen self.title
