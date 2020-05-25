edcfbbfe579430f762cf0b02c3cebd72293925a4
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-25 20:55:44 +0530

    Other files

--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Notes(models.Model):
 	user  = models.ForeignKey(User,on_delete = models.CASCADE)
 	title = models.CharField(max_length=100)
 	content = models.TextField()
-	date_created = models.DateTimeField(default=timezone.now,auto_now_add = True)
-	date_modified = models.DateTimeField(default=timezone.now,auto_now = True)
+	date_created = models.DateTimeField(auto_now_add = True)
+	date_modified = models.DateTimeField(auto_now = True)
 
 	def __str__(self):
-		retuen self.title
+		return self.title
