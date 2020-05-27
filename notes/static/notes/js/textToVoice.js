0854cd07c325477e0f17e7f6a46c7c71cb71a565
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-27 20:17:32 +0530

    Added speech synthesis js code

--- notes/static/notes/js/textToVoice.js
+++ notes/static/notes/js/textToVoice.js
@@ -9,4 +9,27 @@ $(".play-voice").click(function(e){
   		console.log(result)
   }});
 });
+
+const synth = window.speechSynthesis
+let voices = []
+const voiceSelect = document.getElementById('voice-select')
+const getVoices = ()=> {
+	voices = synth.getVoices()
+	console.log(voices)
+	voices.forEach(voice =>{
+		const option = document.createElement('option')
+		option.textContent = voice.name + '(' + voice.lang +')'
+		option.setAttribute('data-lang',voice.lang)
+		option.setAttribute('data-name',voice.name)
+		voiceSelect.appendChild(option)
+		console.log('here')
+	})
+
+}
+getVoices()
+
+if(synth.onvoicechanged !== undefined){
+	synth.onvoicechanged = getVoices;
+}
+
 })
