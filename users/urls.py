dc7da2abd3cc1cc766d6fdd77b6d56a405909b51
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-30 23:35:37 +0530

    Updated urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views 
 
 urlpatterns = [
 		path('SignUp',views.signup,name='user-signup'),
+		# path('SignIn',views.signin,name='user-signin'),
+		path('SignIn',auth_views.LoginView.as_view(template_name='users/signin.html'),name='user-signin'),
+		path('LogOut',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='user-logout'),
+		path('Profile',views.profile,name='user-profile')
 ]
