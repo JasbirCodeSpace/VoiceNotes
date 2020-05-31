09e7728c46ab89589c74c42cc54d1b81c6db97f5
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-31 12:34:34 +0530

    Updated user profile media settings

--- Voice_Based_Web_Browsing/urls.py
+++ Voice_Based_Web_Browsing/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -22,3 +25,5 @@ urlpatterns = [
 	path('user/',include('users.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
