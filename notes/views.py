59f1c87adb3efc7ef5ee1170aca81197043c6b29
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-01 12:53:47 +0530

    Updated views for note creation

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,45 @@
-from django.shortcuts import render
-from .models import Note
 import os
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Note
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
+from django.http import JsonResponse
+from .forms import NoteForm
 
 def home(request):
-	context = {
-		'notes':Note.objects.all()
-	}
-	return render(request,'notes/base.html',context)
+	if request.user.is_authenticated:
+		notes = Note.objects.filter(user=request.user)
+		if request.method == "POST":
+			form = NoteForm(request.POST)
+			if form.is_valid():
+				form_data = form.save(commit=False)
+				form_data.user = request.user
+				form_data.save()
+				return redirect('notes-home')
+		else:		
+			form = NoteForm()
+
+		context ={
+			'notes' : notes,
+			'new_note_form' : form,
+		}
+		return render(request,'notes/notes.html',context)
+	else:
+		return redirect('user-signin')
+
+def create_note(request):
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			form = NoteForm(request.POST)
+			if form.is_valid():
+				form_data = form.save(commit=False)
+				form_data.user = request.user
+				form_data.save()
+		else:
+			form = NoteForm()
+			context = {'form': form}
+		return render(request ,'notes/notes.html',context)
+	else:
+		return render(request,'users/login.html')
 
 # @csrf_exempt
 # def play_notes(request):
