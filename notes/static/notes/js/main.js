788c4122e9c702a68b2a49c080deafa4925d9a18
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-01 17:21:59 +0530

    main script updation

--- notes/static/notes/js/main.js
+++ notes/static/notes/js/main.js
@@ -1,4 +1,26 @@
 $(function () {
-$(".create-note").modalForm({formURL: "{% url 'notes/create-note' %}", modalID: "#create-modal"});
+// $("#create-note").modalForm({formURL: "{% url 'notes/create-note' %}", modalID: "#create-modal"});
+$('.edit-note-button').on('click',function(){
+	$.ajax({
+		url : $(this).attr('data-url'),
+		method:'GET',
+		success:function(data){
+			$('#edit-note-modal-outer-div').html(data)
+			$('#edit-note').modal()
+		}
 
+	})
+})
+
+$('.delete-note-button').on('click',function(){
+
+	$.ajax({
+		url : $(this).attr('data-url'),
+		method: 'GET',
+		success: function(data){
+			$('#delete-note-modal-outer-div').html(data)
+			$('#delete-note').modal()
+		}
+	})
+})
 });
