ab13ef2cbd698a7bbd6acdaf5063cfb5f540f6c2
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-03 20:09:46 +0530

    Added notes filter function

--- notes/static/notes/js/main.js
+++ notes/static/notes/js/main.js
@@ -39,3 +39,20 @@ $('.delete-note-button').on('click',function(){
 	})
 })
 });
+
+function filterNotes() {
+    var input, filter, cards, cardContainer, h5, title, i;
+    input = document.getElementById("noteFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("notesContainer");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body h5.card-title");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
